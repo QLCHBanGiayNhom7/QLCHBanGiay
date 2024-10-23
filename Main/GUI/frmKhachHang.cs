a3ed8bb0f5455b415d128573015ca01c9910df06
PhatNguyen1 <100253089+PhatNguyen1@users.noreply.github.com>
2024-10-24 00:49:53 +0700

    Frm QL Khách Hàng

---
+++ Main/GUI/frmKhachHang.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main.GUI
+{
+    public partial class frmKhachHang : Form
+    {
+        public frmKhachHang()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
