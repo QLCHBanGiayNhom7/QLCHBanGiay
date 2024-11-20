486519003e38d616ecefa58bc76a6ff941a630c3
kimphuongnt <kimphuongnt.dev@gmail.com>
2024-11-20 19:37:33 +0700

    add dbml

---
+++ Main/GUI/frmTrangChu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sunny.UI;
+namespace Main.GUI
+{
+    public partial class frmTrangChu : UIAsideHeaderMainFrame
+    {
+        private string taiKhoan;
+        public string TaiKhoan
+        {
+            get { return taiKhoan; }
+        }
+        public frmTrangChu(string taiKhoan)
+        {
+            InitializeComponent();
+            this.taiKhoan = taiKhoan;
+            KiemTraQuyen();
+        }
+       
+    }
+}
