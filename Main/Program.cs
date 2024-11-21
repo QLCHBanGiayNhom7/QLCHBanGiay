8dc1c151b61ca032dc73c8c1806a83ecab3ada63
huyhaihuoc2411 <huyhaihuoc2411@gmail.com>
2024-11-21 22:49:01 +0700

    Design

--- Main/Program.cs
+++ Main/Program.cs
@@ -25,7 +25,7 @@ namespace Main
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmTrangChu("a", "a"));
+            Application.Run(new frmTrangChu("a","a"));
         }
     }
 }
