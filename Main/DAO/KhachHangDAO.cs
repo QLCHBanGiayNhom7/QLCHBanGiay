eb6ac608553e6d6f6064539415dc65d08f71f3de
PhatNguyen1 <100253089+PhatNguyen1@users.noreply.github.com>
2024-11-29 02:48:25 +0700

    Form Khách Hàng

--- Main/DAO/KhachHangDAO.cs
+++ Main/DAO/KhachHangDAO.cs
@@ -1,40 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Main.DTO;
-
+using Main.DTO; 
 namespace Main.DAO
 {
-    internal class KhachHangDAO
+    public class KhachHangDAO
     {
-        private string connectionString = "Server=DESKTOP-AQ2QICV\\SQLEXPRESS;Database=db_qlshopBanGiay;Trusted_Connection=True;";
-
-        public KhachHangDTO GetKhachHangByMaKH(string maKH)
+        private dbQLBanGiayDataContext db = new dbQLBanGiayDataContext();
+        public List<KhachHangDTO> GetAllKhachHang()
         {
-            string query = "SELECT MaKH, TenKH, SoDienThoai,NgaySinh, GioiTinh FROM KhachHang WHERE MaKH = @MaKH";
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            var khachHangs = db.KhachHangs.Select(kh => new KhachHangDTO
             {
-                SqlCommand cmd = new SqlCommand(query, conn);
-                cmd.Parameters.AddWithValue("@MaKH", maKH);
+                MaKH = kh.MaKH,
+                TenKH = kh.TenKH,
+                SoDienThoai = kh.SoDienThoai,
+                NgaySinh = kh.NgaySinh ?? DateTime.Now,
+                GioiTinh = kh.GioiTinh
+            }).ToList();
 
-                conn.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-                if (reader.Read())
-                {
-                    return new KhachHangDTO
-                    {
-                        MaKH = reader["MaKH"].ToString(),
-                        TenKH = reader["TenKH"].ToString(),
-                        NgaySinh = reader["NgaySinh"].ToString(),
-                        GioiTinh = reader["GioiTinh"].ToString(),
-                        SoDienThoai = reader["SoDienThoai"].ToString()
-                    };
-                }
-            }
-            return null;
+            return khachHangs;
         }
+        public bool AddKhachHang(KhachHangDTO khachHangDTO)
+        {
+            try
+            {
+                var khachHang = new KhachHang
+                {
+                    TenKH = khachHangDTO.TenKH,
+                    SoDienThoai = khachHangDTO.SoDienThoai,
+                    NgaySinh = khachHangDTO.NgaySinh,
+                    GioiTinh = khachHangDTO.GioiTinh
+                };
+
+                db.KhachHangs.InsertOnSubmit(khachHang);
+                db.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool UpdateKhachHang(KhachHangDTO khachHangDTO)
+        {
+            try
+            {
+                var khachHang = db.KhachHangs.FirstOrDefault(kh => kh.MaKH == khachHangDTO.MaKH);
+                if (khachHang != null)
+                {
+                    khachHang.TenKH = khachHangDTO.TenKH;
+                    khachHang.SoDienThoai = khachHangDTO.SoDienThoai;
+                    khachHang.NgaySinh = khachHangDTO.NgaySinh;
+                    khachHang.GioiTinh = khachHangDTO.GioiTinh;
+
+                    db.SubmitChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool DeleteKhachHang(int maKH)
+        {
+            try
+            {
+                var khachHang = db.KhachHangs.FirstOrDefault(kh => kh.MaKH == maKH);
+                if (khachHang != null)
+                {
+                    db.KhachHangs.DeleteOnSubmit(khachHang);
+                    db.SubmitChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+
     }
 }
